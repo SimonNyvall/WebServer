8009589545bfd60313f9853cbbffa3864f0de055
NeotericSh <simon.k.nyvall@proton.me>
2024-07-27 00:25:44 +0200

    chore: Add ServerTestHelper class for integration tests

---
+++ tests/IntegrationTests/ServerTestHelper.cs
@@ -0,0 +1,66 @@
+using System.Diagnostics;
+
+namespace IntegrationTests;
+
+public class ServerTestHelper : IDisposable
+{
+    public string ServerUrl => "localhost";
+    public int ServerPort => 6001;
+    public string ServerAddress => $"http://{ServerUrl}:{ServerPort}";
+    private bool _disposed = false;
+    private Process? _serverProcess;
+
+    public ServerTestHelper() 
+    { 
+        string workingDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../..", "src"));
+
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = "dotnet",
+            Arguments = "run",
+            WorkingDirectory = workingDirectory,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        };
+
+        Task.Run(() => StartServer(startInfo));
+    }
+
+    private Task StartServer(ProcessStartInfo startInfo)
+    {
+        _serverProcess = Process.Start(startInfo);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~ServerTestHelper()
+    {
+        Dispose(false);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed) return;
+
+        if (!disposing) return;
+
+        if (_serverProcess is not null && !_serverProcess.HasExited)
+        {
+            _serverProcess.Kill();
+            _serverProcess.WaitForExit();
+            _serverProcess.Dispose();
+        }
+
+        _disposed = true;
+    }
+
+    
+}
--- tests/IntegrationTests/ServerTests.cs
+++ tests/IntegrationTests/ServerTests.cs
@@ -12,67 +12,9 @@ public class ServerTests
         using var serverHelper = new ServerTestHelper();
 
         var client = new HttpClient();
-        var response = await client.GetAsync(serverHelper.ServerUrl);
+        var response = await client.GetAsync(serverHelper.ServerAddress);
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
-}
+ }
 
-public class ServerTestHelper : IDisposable
-{
-    public string ServerUrl => "http://localhost:6001";
-    private bool _disposed = false;
-    private Process? _serverProcess;
-
-    public ServerTestHelper() 
-    { 
-        string workingDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../..", "src"));
-
-        ProcessStartInfo startInfo = new()
-        {
-            FileName = "dotnet",
-            Arguments = "run",
-            WorkingDirectory = workingDirectory,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true
-        };
-
-        Task.Run(() => StartServer(startInfo));
-    }
-
-    private Task StartServer(ProcessStartInfo startInfo)
-    {
-        _serverProcess = Process.Start(startInfo);
-
-        return Task.CompletedTask;
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (_disposed) return;
-
-        if (!disposing) return;
-
-        if (_serverProcess is not null && !_serverProcess.HasExited)
-        {
-            _serverProcess.Kill();
-            _serverProcess.WaitForExit();
-            _serverProcess.Dispose();
-        }
-
-        _disposed = true;
-    }
-
-    ~ServerTestHelper()
-    {
-        Dispose(false);
-    }
-}
